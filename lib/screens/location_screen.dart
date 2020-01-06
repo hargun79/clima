5ebe7a0ba072214b674d6eb1f9b7a32f134b302f
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-01-06 22:51:00 +0530

    Update location_screen.dart

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -31,7 +31,7 @@ class _LocationScreenState extends State<LocationScreen> {
         cityName = '';
         return;
       }
-      double temp = weatherData['main']['temp'];
+      dynamic temp = weatherData['main']['temp'];
       temperature = temp.toInt();
       var condition = weatherData['weather'][0]['id'];
       cityName = weatherData['name'];
